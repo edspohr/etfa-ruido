dd898b7fbca6455a774850090e654ce3f47d4efd
edspohr <edmundo@spohr.cl>
2025-12-31 11:57:27 -0300

    Fix: Push missing useCallback fix for ProjectDetails

--- src/pages/AdminProjectDetails.jsx
+++ src/pages/AdminProjectDetails.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import Layout from '../components/Layout';
 import { db } from '../lib/firebase';
